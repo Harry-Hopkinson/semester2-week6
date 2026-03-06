35bc92626005e0f622b3865838607920caf414ac
Harry Hopkinson <63599884+Harry-Hopkinson@users.noreply.github.com>
2026-03-06 09:07:08 +0000

    Complete Task 3

---
+++ session1/task3/word_count.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+  if (argc < 2)
+  {
+    printf("Usage:\n");
+    printf("%s filename\n", argv[0]);
+    return 1;
+  }
+
+  FILE *file = fopen(argv[1], "r");
+  if (file == NULL)
+  {
+    perror("Error opening file");
+    return 1;
+  }
+
+  int lines = 0, words = 0, characters = 0;
+  char c;
+
+  while ((c = fgetc(file)) != EOF)
+  {
+    characters++;
+    if (c == '\n')
+      lines++;
+    if (c == ' ' || c == '\n' || c == '\t')
+      words++;
+  }
+
+  if (characters > 0 && (c != ' ' && c != '\n' && c != '\t'))
+    words++;
+
+  fclose(file);
+
+  printf("Lines: %d\n", lines);
+  printf("Words: %d\n", words);
+  printf("Characters: %d\n", characters);
+
+  return 0;
+}
